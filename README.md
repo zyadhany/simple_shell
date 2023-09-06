210fdb500b9e1080ef1683e32d7b32cd7a78fefa
zyadhany <zeyad.hany2003@gmail.com>
2023-09-06 20:14:45 +0300

    Setting everythings up

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo for shell project in alx programme.
---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int main(){
+    char *command = NULL;
+
+    while (1)
+    {
+        get_Command(command);
+
+ 
+    }
+    
+
+    
+    return (0);
+}
