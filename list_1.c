cdb3169aad1f77fb22259c3feb8c7163574346a9
OllyBabs <oladimejisheriff001@gmail.com>
2023-09-14 17:02:33 +0100

    code edit 03

--- list_1.c
+++ list_1.c
@@ -1,13 +1,10 @@
 #include "s_shell.h"
 
-
 /**
- * print_list - print list
- * @lis: given list
- *
- * Return: size of list.
+ * print_list - prints a list
+ * @lis: list given
+ * Return: size of list
  */
-
 size_t print_list(const list_t *lis)
 {
 	int n = 0;
@@ -23,14 +20,11 @@ size_t print_list(const list_t *lis)
 	return (n);
 }
 
-
 /**
- * list_len - get lenght of given list.
- * @lis: given list.
- *
- * Return: size of list.
+ * list_len - gets the length of a given list
+ * @lis: list given
+ * Return: size of list
  */
-
 size_t list_len(const list_t *lis)
 {
 	int n = 0;
@@ -44,19 +38,16 @@ size_t list_len(const list_t *lis)
 	return (n);
 }
 
-
 /**
- * add_node - add node to the list.
- * @lis: given list.
- * @s: added element.
- *
- * Return: pointer to list.
+ * add_node - adds a new node to a given list
+ * @lis: list given
+ * @s: added element
+ * Return: pointer to list
  */
 
 list_t *add_node(list_t **lis, char *s)
 {
 	list_t *tmp = malloc(sizeof(list_t)), *at = *lis;
-
 
 	if (!tmp)
 		return (NULL);
@@ -88,11 +79,11 @@ list_t *add_node(list_t **lis, char *s)
 
 
 /**
- * delete_node_at_index - delete index of node in list
- * @lis: given list
+ * delete_node_at_index - deletes node 
+ * at a specified index in list
+ * @lis: list given
  * @k: index of list
- *
- * Return: pointer to list.
+ * Return: pointer to list
  */
 
 int delete_node_at_index(list_t **lis, int k)
