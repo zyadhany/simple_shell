9343f5ba798fd2a2ebb124b6de2fa5cdf5cd5aae
zyadhany <zeyad.hany2003@gmail.com>
2023-09-20 20:08:15 +0300

    changed error massage

--- execute_Command.c
+++ execute_Command.c
@@ -8,7 +8,7 @@
 int execute_Command(void)
 {
 	int id, i;
-	char *error_message[3] = {"No such file or directory", NULL};
+	char *error_message[3] = {"not found", NULL};
 
 	for (i = 0; info.functions[i].name; i++)
 		if (_strcomp(info.command[0], info.functions[i].name) == 0)
