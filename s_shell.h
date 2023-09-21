0b23858e40097f57fc2d782e721bbc9834d762ac
OllyBabs <oladimejisheriff001@gmail.com>
2023-09-21 07:11:21 +0100

    Adjusting header

--- s_shell.h
+++ s_shell.h
@@ -2,13 +2,13 @@
 #define S_SHELL
 
 #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <unistd.h>
 
 /*Global variables*/
 extern char **environ;
